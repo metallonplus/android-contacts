d7631d4c6793884155660336ba4c6383c6620f24
metallonplus <metallonplus@yandex.ru>
2025-11-25 14:53:17 +0300

    Update version 2 проектная работа

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
@@ -4,5 +4,5 @@ import androidx.annotation.NonNull;
 
 public interface ListDiffInterface<T> {
     boolean theSameAs(@NonNull T other);
-    boolean equals(Object o);// новый пуш
+    boolean equals(Object o);
 }
